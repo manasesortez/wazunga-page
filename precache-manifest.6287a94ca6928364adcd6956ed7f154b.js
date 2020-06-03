16f1e718070828a5248a31781ba4f596eea2f60e
nhulox97 <sergiobernal909@gmail.com>
2020-06-03 12:21:55 -0600

    Updates

---
+++ precache-manifest.6287a94ca6928364adcd6956ed7f154b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4a56d77d108fc2f3f5c0ab83f6e91c",
+    "url": "/wazunga-page/index.html"
+  },
+  {
+    "revision": "0b406252a1c3218a9bf0",
+    "url": "/wazunga-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b72944a0e20580b43c6f",
+    "url": "/wazunga-page/static/js/2.109b6bde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wazunga-page/static/js/2.109b6bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b406252a1c3218a9bf0",
+    "url": "/wazunga-page/static/js/main.6195da9a.chunk.js"
+  },
+  {
+    "revision": "aa416d419e78fd5e376c",
+    "url": "/wazunga-page/static/js/runtime-main.488f3d88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wazunga-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
